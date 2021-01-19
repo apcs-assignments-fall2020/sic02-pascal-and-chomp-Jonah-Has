7c64f86bffbe9bd77308ef44e990de75b58a8f35
Jonah-Has <23jhasegawa@athenian.org>
2021-01-19 17:06:07 +0000

    wrote the code for pascal

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -7,7 +7,18 @@
 public class Pascal
 {
     public static int[][] pascalTriangle(int n) {
-        // YOUR CODE HERE
-        return null;
+        int[][] pascal = new int[n][n];
+        for (int item: pascal[0]){
+            pascal[0][item] = 0;
+        }
+        for (int i = 0; i < n; i++){
+            pascal[i][0] = 1;
+        }
+        for (int row = 1; row < n; row++){
+            for (int col = 1; col < n; col++){
+                pascal[row][col] = pascal[row - 1][col] + pascal[row - 1][col - 1];
+            }
+        }
+        return pascal;
     }
 }
